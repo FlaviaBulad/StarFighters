58ca2d7efcf8c9bd6c907bdb83455929d580ee88
Flávia Bulad <flavia.bulad@gmail.com>
2022-08-31 18:50:17 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import cors from 'cors';
+import "express-async-errors";
+import dotenv from "dotenv";
+
+import router from "./routes/index.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors(), json());
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

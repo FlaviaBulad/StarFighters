585f5d8eacc19a19d47719115808acf8f3a1356d
Flávia Bulad <flavia.bulad@gmail.com>
2022-08-31 20:05:02 -0300

    fix: next type

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -1,8 +1,8 @@
-import {Request, Response} from 'express';
+import {Request, Response, NextFunction} from 'express';
 import {Schema} from 'joi';
 
 export default function validateSchema(schema: Schema){
-    return (req: Request, res: Response, next: Function) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         const { error } = schema.validate(req.body, { abortEarly: false });
         if (error) {
           return res.status(422).send(error.details.map((detail) => detail.message));
